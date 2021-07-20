c043d2f9e6f9f915311fb69722cdf974f2e33b60
jaabir <jaabir.zaid@gmail.com>
2021-07-20 22:22:35 +0530

    asdasd

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from os import error
 from flask import Flask, render_template, request, url_for
 from model import predict
+import pickle
 
 # app = Flask('__main__')
 app = Flask(__name__, template_folder='templates')
 print(f'templates folder path : {app.template_folder}')
+with open('rfcModel','rb') as file:
+    clf = pickle.load(file)
 
 @app.route('/', methods = ['GET'])
 def home():
@@ -14,7 +17,7 @@ def home():
     if request.args:
         url = request.args['url_input']
         if 'http' in url:
-            res = predict(url)
+            res = predict(url, clf)
             base_url = url.split('/')[2]
             error = 1 if res == -1 else 0
         else:
@@ -23,4 +26,4 @@ def home():
     return render_template('index.html', result = res, fname = 'index.css', error = error, url = base_url)
 
 if __name__ == "__main__":
-    app.run(threaded = True)
+    app.run(threaded = True, debug=True)
--- model.py
+++ model.py
@@ -1,30 +1,25 @@
 from features.feature_extractor import extract_features as ef
 import pandas as pd 
 import numpy as np
-# from sklearn.preprocessing import RobustScaler as rbs
-# from sklearn.ensemble import RandomForestClassifier as rfc
-from features.feature_extractor import extract_features as ef
 import pickle
 import warnings
 
 
 warnings.filterwarnings('ignore')
-# rb_scaler = rbs()
 df = pd.read_csv('dataset_phishing.csv')
 
 def get_features(url):
     x = ef(url, 0)
     x = x[1:47] + x[50:-1] if x is not None else None
+    print(x)
     return x
 
-def pred(x):
+def pred(x, clf):
     x = np.array(x).reshape(1,-1) 
-    with open('rfcModel','rb') as file:
-        clf = pickle.load(file)
     
     return clf.predict(x)    
 
-def predict(url):
+def predict(url, clf):
     replace = {
         'legitimate'  : 0,
         'phishing' : 1
@@ -32,11 +27,14 @@ def predict(url):
 
     result = df.status[df.url == url].replace(replace)
     if len(result) > 0:
-        result = True if result[0] == 1 else False
+        try:
+            result = True if result.values[0] == 1 else False
+        except KeyError as error:
+            print('its an error :', error)
     else:
         x = get_features(url)
         if x == None or len(x) != 83 : return -1
-        result = True if pred(x)[0] == 'legitimate' else False
+        result = True if pred(x, clf)[0] == 'legitimate' else False
     return result
 
     
